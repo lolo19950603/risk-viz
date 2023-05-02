938206077e8251fbd89a6daf56f854192718639c
loren950603 <loren840603@gmail.com>
2023-05-03 01:04:07 +0800

    Change img tag to Image<> for better performance

--- src/components/MainComponents/Map/ColorIndicator/ColorIndicator.tsx
+++ src/components/MainComponents/Map/ColorIndicator/ColorIndicator.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function ColorIndicator() {
   return (
     <ul className="flex justify-start gap-4 pb-1 text-black">
@@ -7,19 +9,19 @@ export default function ColorIndicator() {
         </span>
       </li>
       <li className="flex">
-        <img src="images/green.png" height="32" width="32" />
+        <Image src="/images/green.png" alt='0.40-0.45' height="32" width="32" />
         <span className="pt-1">0.40-0.45</span>
       </li>
       <li className="flex">
-        <img src="images/yellow.png" height="32" width="32" />
+        <Image src="/images/yellow.png" alt='0.45-0.50' height="32" width="32" />
         <span className="pt-1">0.45-0.50</span>
       </li>
       <li className="flex">
-        <img src="images/orange.png" height="32" width="32" />
+        <Image src="/images/orange.png" alt='0.50-0.55' height="32" width="32" />
         <span className="pt-1">0.50-0.55</span>
       </li>
       <li className="flex">
-        <img src="images/red.png" height="32" width="32" />
+        <Image src="/images/red.png" alt='0.55-0.60' height="32" width="32" />
         <span className="pt-1">0.55-0.60</span>
       </li>
     </ul>
